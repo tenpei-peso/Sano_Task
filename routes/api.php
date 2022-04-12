ee5011a8071b7cc40c6cc1b4b6ea54208ebcdabd
tenpei-peso <kikichan.fps@gmail.com>
2022-04-12 11:29:55 +0000

    fix task2

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,4 @@ Route::get('/member_list/{area?}', [MemberController::class, 'showMemberList']);
 
 Route::get('/member_detail/{id}', [MemberController::class, 'show']); //chap7
 
-Route::get('/member_detail', [MemberController::class, 'index']); //chap7
-
 Route::post('/search_members', [MemberController::class, 'searchMembers']);
