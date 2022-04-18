68cb287255143ff859467b069f35864c38b60f96
tenpei-peso <kikichan.fps@gmail.com>
2022-04-18 15:20:42 +0900

    エラー制御の課題です

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+
 
 use function PHPUnit\Framework\isEmpty;
 
@@ -48,56 +51,98 @@ class Member extends Model
     ];
     
     public function getIdUser(){
-        $user = $this->where('id', 1)->get();
-        return $user;
+        try {
+            $user = $this->where('id', 1)->get();
+            return $user;
+
+        } catch (\Exception $e){
+            Log::emergency($e->getMessage());
+            throw $e;
+        }
     }
 
     public function getAreaUser(){
-        $user = $this->where('area', '東京')->get();
-        return $user;
+        try {
+            $user = $this->where('area', '東京')->get();
+            return $user;
+
+        } catch (\Exception $e){
+            Log::emergency($e->getMessage());
+            throw $e;
+        }
     }
 
     public function getAgeUser(){
-        $user = $this->where('age', '<=', 30)->get();
-        return $user;
+        try {
+            $user = $this->where('age', '<=', 30)->get();
+            return $user;
+
+        } catch (\Exception $e){
+            Log::emergency($e->getMessage());
+            throw $e;
+        }
     }
 
     public function allUser() {
-        $user = $this->all();
-        return $user;
+        try {
+            $user = $this->all();
+            return $user;
+
+        } catch (\Exception $e){
+            Log::emergency($e->getMessage());
+            throw $e;
+        }
     }
 
-    //<------  07 step1 ---------->
-    public function findIdUser($id) {
-        $findUser = $this->find($id);
-        return $findUser;
+    // <------  07 step1 ---------->
+    public function findIdUse($id) {
+        try {
+            $findUser = $this->find($id);
+            return $findUser;
+        } catch (\Exception $e){
+            Log::emergency($e->getMessage());
+            throw $e;
+        }
     }
 
     //<------  07 step3 ---------->
     public function findAreaMembers($area) {
-        if ($area == !null) {
-            $members = $this->where('area', $area)->get();
-        } else {
-            $members = $this->all();
+        try {
+            if ($area == !null) {
+                $members = $this->where('area', $area)->get();
+            } else {
+                $members = $this->all();
+            }
+            return $members;
+            
+        } catch (\Exception $e){
+            Log::emergency('props内容: . $area');
+            Log::emergency($e->getMessage());
+            throw $e;
         }
-
-        return $members;
     }
 
     //<------  07 step5 & 6 ---------->
     public function searchSelectMembers($minAgeData = null, $maxAgeData = null) {
-        $query = $this->query();
+        try {
+            $query = $this->query();
 
-        if($minAgeData != null) {
-            $query->where('age', '>=', $minAgeData);
+            if($minAgeData != null) {
+                $query->where('age', '>=', $minAgeData);
+            }
+
+            if($maxAgeData != null) {
+                $query->where('age', '<=', $maxAgeData);
+            }
+
+            $members = $query->get();
+            return $members;
+
+        } catch (\Exception $e){
+            Log::emergency('props内容: . $minAgeData . $maxAgeData');
+            Log::emergency($e->getMessage());
+            throw $e;
         }
-
-        if($maxAgeData != null) {
-            $query->where('age', '<=', $maxAgeData);
-        }
-
-        $members = $query->get();
-        return $members;
     }
 
 }
