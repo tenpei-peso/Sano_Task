207568f836ae8ce1e3a5981e8de55c06df12ecf2
tenpei-sano <kikichan.fps@gmail.com>
2022-04-25 17:08:19 +0900

    seederを一括でinsert

--- database/seeders/PracticeSeeder.php
+++ database/seeders/PracticeSeeder.php
@@ -17,59 +17,51 @@ class PracticeSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('practices')->insert(
+        $insert = [
             [
                 'team_id' => 1,
                 'location' => "京セラドーム",
                 'date' => '20220317',
                 'start_time' => '15:30',
                 'finish_time' => '18:00',
-            ]);
-
-        DB::table('practices')->insert(
+            ],
             [
                 'team_id' => 1,
                 'location' => "通天閣",
                 'date' => '20220319',
                 'start_time' => '16:00',
                 'finish_time' => '19:00',
-            ]);
-
-        DB::table('practices')->insert(
+            ],
             [
                 'team_id' => 2,
                 'location' => "京セラドーム",
                 'date' => '20220418',
                 'start_time' => '15:00',
                 'finish_time' => '18:00',
-            ]);
-
-        DB::table('practices')->insert(
+            ],
             [
                 'team_id' => 3,
                 'location' => "甲子園",
                 'date' => '20220510',
                 'start_time' => '15:30',
                 'finish_time' => '18:00',
-            ]);
-
-        DB::table('practices')->insert(
+            ],
             [
                 'team_id' => 4,
                 'location' => "幼稚園",
                 'date' => '20220601',
                 'start_time' => '15:30',
                 'finish_time' => '17:00',
-            ]);
-
-        DB::table('practices')->insert(
+            ],
             [
                 'team_id' => 4,
                 'location' => "小学校",
                 'date' => '20220603',
                 'start_time' => '16:30',
                 'finish_time' => '19:00',
-            ]);
+            ]
+        ];
+        DB::table('practices')->insert($insert);
     }
     
 }
